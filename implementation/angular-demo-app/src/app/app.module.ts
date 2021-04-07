3e16fcaeabb039b2b0901c39f266bf9487b655a7
Dominic Luidold <dominic.luidold@students.fhv.at>
2021-04-07 18:26:54 +0200

    Add general PWA support

--- implementation/angular-demo-app/src/app/app.module.ts
+++ implementation/angular-demo-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { EntranceControlComponent } from '@app/entrance-control/entrance-control.component';
@@ -11,6 +12,7 @@ import { HomeComponent } from '@app/home/home.component';
 import { MaterialModule } from '@app/material-module';
 import { NavbarComponent } from '@app/navbar/navbar.component';
 import { PhotoManagerComponent } from '@app/photo-manager/photo-manager.component';
+import { environment } from '@environments/environment';
 import { AttendeeColumnComponent } from './attendee-column/attendee-column.component';
 
 @NgModule({
@@ -29,7 +31,13 @@ import { AttendeeColumnComponent } from './attendee-column/attendee-column.compo
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
-    RouterModule
+    RouterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
